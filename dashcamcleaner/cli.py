543389444ad4d31672f0163e22621d630b7c1ead
tfaehse <tfaehse@me.com>
2023-04-02 21:32:36 +0200

    Introduce YOLOv8 models (#64)
    - replace all YOLOv5 models
    - always use inference size == training size
    - show status in GUI (which stage the pipeline is currently in)
    - rewrite blurring algorithm, optimizing memory and CPU footprint
    - allow a few more output container types, support depends on system
    - silence FFMPEG macro block size errors
    - remove colored mask option (for now) :(

--- dashcamcleaner/cli.py
+++ dashcamcleaner/cli.py
@@ -6,6 +6,7 @@ import sys
 import textwrap
 from pathlib import Path
 from typing import Dict, List, Union
+import re
 
 from src.blurrer import VideoBlurrer
 
@@ -15,10 +16,17 @@ signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 class CLI:
     def __init__(self, opt) -> None:
+        """
+        Class for blurring videos from a CLI
+        :param opt: set of input arguments
+        """
         self.opt = opt
         self.sanitize_opts()
 
     def sanitize_opts(self):
+        """
+        Sanity check for paths in input arguments
+        """
         input_path, output_path = Path(self.opt.input_path), Path(self.opt.output_path)
         if input_path.is_file() and output_path.is_dir():  # if input refers a file, output must refer to a file too
             self.opt.output_path = (output_path / input_path.name).absolute()
@@ -27,20 +35,23 @@ class CLI:
             sys.exit(f'The output_path "{self.opt.output_path}" already exists. The file will not be overwritten.')
         if input_path.is_dir():  # batch processing mode
             if not output_path.is_dir():
-                sys.exit('For batch processing mode, both input_path and output_path must be directories!')
-            if not any(input_path.glob('*.*')):
-                sys.exit('The input_path is empty. Nothing to do.')
-            for input_file in input_path.glob('*.*'):
+                sys.exit("For batch processing mode, both input_path and output_path must be directories!")
+            if not any(input_path.glob("*.*")):
+                sys.exit("The input_path is empty. Nothing to do.")
+            for input_file in input_path.glob("*.*"):
                 test_out_path = output_path / input_file.name
                 if test_out_path.exists():
                     sys.exit(f'The output_path "{test_out_path.absolute()}" already exists. Aborting.')
         elif not input_path.is_file():
-            sys.exit('input_path is invalid')
+            sys.exit("input_path is invalid")
 
     def start_blurring(self):
+        """
+        Start the blurring process(es)
+        """
         input_path, output_path = Path(self.opt.input_path), Path(self.opt.output_path)
         if input_path.is_dir():  # batch mode
-            for input_file in sorted(input_path.glob('*.*')):
+            for input_file in sorted(input_path.glob("*.*")):
                 self.opt.input_path = input_file.absolute()
                 self.opt.output_path = (output_path / input_file.name).absolute()
                 self.start_blurring_file()
@@ -48,15 +59,19 @@ class CLI:
             self.start_blurring_file()
 
     def start_blurring_file(self):
-        print('Start blurring video:', self.opt.input_path)
-        print('Blurring parameter:', vars(self.opt))
+        """
+        Blur a single video file
+        """
+        print("Start blurring video:", self.opt.input_path)
+        print("Blurring parameter:", vars(self.opt))
 
         # set up parameters
         parameters: Dict[str, Union[bool, int, float, str]] = vars(self.opt)  # convert opt to dict type
 
         # read inference size
-        inference_size = int(self.opt.inference_size) * 16 / 9  # ouch again
-        parameters["inference_size"] = inference_size  # ouch again indeed...
+        model_name: str = parameters["weights"]
+        training_inference_size: int = int(re.search(r"(?P<imgsz>\d*)p\_", model_name).group("imgsz"))
+        parameters["inference_size"] = int(training_inference_size * 16 / 9)
 
         # setup blurrer
         blurrer = VideoBlurrer(self.opt.weights, parameters)
@@ -66,13 +81,18 @@ class CLI:
 
 
 def parse_arguments():
+    """
+    Argument parser
+    :return: set of parsed arguments
+    """
+
     class CustomHelpFormatter(argparse.HelpFormatter):
-        def __init__(self: 'CustomHelpFormatter', prog) -> None:
+        def __init__(self: "CustomHelpFormatter", prog) -> None:
             super().__init__(prog, max_help_position=8)
             # import re
             # self._whitespace_matcher = re.compile(r'[.]+', re.ASCII)
 
-        def _format_action_invocation(self: 'CustomHelpFormatter', action):
+        def _format_action_invocation(self: "CustomHelpFormatter", action):
             if not action.option_strings:
                 return "  " + super()._format_action_invocation(action)
             default = self._get_default_metavar_for_optional(action)
@@ -82,7 +102,7 @@ def parse_arguments():
                 default_value = f"  (Default: {action.default})"
             return f"  {action.option_strings[0]} {args_string}{default_value}\n    {action.option_strings[1]} {args_string}"
 
-        def _split_lines(self: 'CustomHelpFormatter', text: str, width: int) -> List[str]:
+        def _split_lines(self: "CustomHelpFormatter", text: str, width: int) -> List[str]:
             ret = []
             for line in text.splitlines():
                 if len(line) > width:
@@ -92,14 +112,14 @@ def parse_arguments():
 
             # add empty line to separate each argument
             if len(ret[-1]) > 0:
-                ret.extend([''])
+                ret.extend([""])
 
             return ret
 
     parser = argparse.ArgumentParser(
         formatter_class=CustomHelpFormatter,
         description="This tool allows you to automatically censor faces and number plates on dashcam footage.",
-        add_help=False  # for custom grouping, at the end after required args
+        add_help=False,  # for custom grouping, at the end after required args
     )
 
     required = parser.add_argument_group("required arguments")
@@ -108,7 +128,7 @@ def parse_arguments():
         "--input_path",
         required=True,
         help="Input video file path. Pass a folder name for batch processing all files in the folder.",
-        type=str
+        type=str,
     )
     required.add_argument(
         "-o",
@@ -129,6 +149,14 @@ def parse_arguments():
         type=str,
         default="720p_medium_mosaic",
     )
+    optional.add_argument(
+        "-bw",
+        "--blur_workers",
+        required=False,
+        help="Amount of processes to use for blurring frames. (default = 2)",
+        type=int,
+        default=2,
+    )
     advanced.add_argument(
         "-s",
         "--batch_size",
@@ -137,24 +165,16 @@ This will read multiple frames at the same time and perform detection on all of 
 Not recommended for CPU usage.""",
         type=int,
         metavar="[1, 1024]",
-        default=1,
+        default=2,
     )
     optional.add_argument(
         "-b",
         "--blur_size",
         required=False,
-        help="Kernel radius of the gauss-filter. Higher value means more blurring, 0 would mean no blurring at all.",
+        help="Kernel radius of the blurring-filter. Higher value means more blurring, 0 would mean no blurring at all.",
         type=int,
         metavar="[1, 99]",
         default=9,
-    )
-    optional.add_argument(
-        "-if",
-        "--inference_size",
-        help="Vertical inference size, e.g. 1080 or 720.",
-        type=int,
-        metavar="[144, 2160]",
-        default=720,
     )
     optional.add_argument(
         "-t",
@@ -224,11 +244,7 @@ Hint: turn off --feather_edges by setting -fe=0 and turn --quality to 10""",
         default=False,
     )
     optional.add_argument(
-        '-h',
-        '--help',
-        action='help',
-        default=argparse.SUPPRESS,
-        help='Show this help message and exit.'
+        "-h", "--help", action="help", default=argparse.SUPPRESS, help="Show this help message and exit."
     )
 
     return parser.parse_args()
